ba7966aac1af668a48bc6a73033e8a728c4091f5
yan-fuhai <yan_fuhai@hotmail.com>
2020-11-30 13:47:14 +0800

    Update license

--- client/parser.go
+++ client/parser.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Fuhai Yan.
+// Copyright (c) 2020 The Houyi Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
